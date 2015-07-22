845613229e9dd300f0b0e0c52ace2ae9cd4d379b
Kaspar Manz <kaspar@xeophin.net>
2015-07-22 17:27:17 +0200

    Added class to handle version numbers.

--- Editor/BuildHelper.cs
+++ Editor/BuildHelper.cs
@@ -4,42 +4,128 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 
-public static class BuildHelper
+namespace Net.Xeophin.Utils.Version
 {
-
-  [MenuItem ("Build/Save Version Information")]
-  public static void SaveVersionInformation ()
+  public static class BuildHelper
   {
-    string companyName;
-    string gameName;
-    string informalVersion;
+    const string bowerPath = "Assets/bower.json";
 
-    using (StreamReader reader = File.OpenText ("Assets/bower.json")) {
-      JObject o = (JObject)JToken.ReadFrom (new JsonTextReader (reader));
+    [MenuItem ("Build/Save Version Information")]
+    public static void SaveVersionInformation ()
+    {
+      string companyName;
+      string gameName;
+      string informalVersion;
 
-      informalVersion = (string)o ["version"];
-      companyName = (string)o ["company"];
-      gameName = (string)o ["name"];
+      if (!File.Exists (bowerPath)) {
+        return;
+      }
+
+      //Check if bower.json exists, else return
+
+      using (StreamReader reader = File.OpenText (bowerPath)) {
+        JObject o = (JObject)JToken.ReadFrom (new JsonTextReader (reader));
+
+        informalVersion = (string)o ["version"];
+        companyName = (string)o ["company"];
+        gameName = (string)o ["name"];
+
+      }
+     
+      SemVer versionObject = new SemVer (informalVersion);
+
+      string fileContents = string.Format (
+                              "using System.Reflection;\n\n" +
+                              "[assembly:AssemblyVersion (\"{0}\")]\n" +
+                              "[assembly:AssemblyCompany (\"{1}\")]\n" +
+                              "[assembly:AssemblyProduct (\"{2}\")]\n" +
+                              "[assembly:AssemblyInformationalVersion (\"{3}\")]",
+                              versionObject.MicrosoftFormat,
+                              companyName,
+                              gameName,
+                              versionObject.CompleteVersionInformation);
+
+      File.WriteAllText ("Assets/AssemblyVersion.cs", fileContents);
+    }
+  }
+
+  /// <summary>
+  /// A semantic version number.
+  /// </summary>
+  public class SemVer
+  {
+    public int Major, Minor, Patch;
+    public string Prerelease, Build;
+
+    public string StableRelease {
+      get {
+        return string.Format ("{0}.{1}.{2}", Major, Minor, Patch);
+      }
+    }
+
+    public string UnstableRelease {
+      get {
+        if (string.IsNullOrEmpty (Prerelease)) {
+          return StableRelease;
+        }
+        return string.Format ("{0}-{1}", StableRelease, Prerelease);
+      }
+    }
+
+    public string CompleteVersionInformation {
+      get {
+        if (string.IsNullOrEmpty (Build)) {
+          return UnstableRelease;
+        }
+        return string.Format ("{0}+{1}", UnstableRelease, Build);
+      }
+    }
+
+    /// <summary>
+    /// Gets the Microsoft format for the version, leaving * for Build and Revision number.
+    /// </summary>
+    /// <value>The microsoft format.</value>
+    public string MicrosoftFormat {
+      get {
+        return string.Format (
+          "{0}.{1}.*.*",
+          Major,
+          Minor);
+      }
+    }
+
+    public override string ToString ()
+    {
+      return CompleteVersionInformation;
+    }
+
+    public SemVer (string versionString)
+    {
+      string[] vAndBuild = versionString.Split ('+');
+
+      if (vAndBuild.Length > 1) {
+        Build = vAndBuild [1];
+      } else {
+        Build = string.Empty;
+      }
+
+      string prereleaseString = vAndBuild [0];
+      string[] vAndPrerelease = prereleaseString.Split ('-');
+
+      if (vAndPrerelease.Length > 1) {
+        Prerelease = vAndPrerelease [1];
+      } else {
+        Prerelease = string.Empty;
+      }
+
+      string pureVersionString = vAndPrerelease [0];
+
+      string[] versionNumbers = pureVersionString.Split ('.');
+
+      Major = int.Parse (versionNumbers [0]);
+      Minor = int.Parse (versionNumbers [1]);
+      Patch = int.Parse (versionNumbers [2]);
 
     }
-     
-
-    string[] parts = informalVersion.Split (new char[]{ '.' });
-    string version = string.Format ("{0}.{1}.*.*", parts [0], parts [1]);
-
-    string fileContents = string.Format (
-                            "using System.Reflection;\n\n" +
-                            "[assembly:AssemblyVersion (\"{0}\")]\n" +
-                            "[assembly:AssemblyCompany (\"{1}\")]\n" +
-                            "[assembly:AssemblyProduct (\"{2}\")]\n" +
-                            "[assembly:AssemblyInformationalVersion (\"{3}\")]",
-                            version,
-                            companyName,
-                            gameName,
-                            informalVersion);
-
-    Debug.Log (fileContents);
-
-    File.WriteAllText ("Assets/AssemblyVersion.cs", fileContents);
   }
 }
